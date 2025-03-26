3c5e43d328b6db06aeba9ae2f95fdde8f5a63c89
zeph254 <ularezephaniah@gmail.com>
2025-03-26 10:58:27 +0300

    moving to windows

---
+++ puddle/item/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Item
+
+INPUT_CLASSES = 'w-full py-3 px-4 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-teal-400 focus:border-teal-400 outline-none'
+
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['category', 'name', 'description', 'price', 'image']
+
+        widgets = {
+            'category': forms.Select(attrs={'class': INPUT_CLASSES}),
+            'name': forms.TextInput(attrs={'class': INPUT_CLASSES}),
+            'description': forms.Textarea(attrs={'class': INPUT_CLASSES}),
+            'price': forms.NumberInput(attrs={'class': INPUT_CLASSES}),
+            'image': forms.FileInput(attrs={'class': INPUT_CLASSES}),
+        }    
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Item
+from .forms import NewItemForm
 
 # Create your views here.
 
@@ -7,3 +9,18 @@ def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk) [0:3]# exclude the current item
     return render(request, 'item/detail.html', {'item': item, 'related_items': related_items})  
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk=item.pk)
+
+    else:
+        form = NewItemForm()
+    form = NewItemForm()
+    return render(request, 'item/form.html', {'form': form, 'title': 'New Item'})
